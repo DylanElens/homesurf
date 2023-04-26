f02b43c3ea043f15b29132ef29f0fcd2e4f30500
Dylan Elens <dylanelens@hotmail.com>
2023-04-26 14:41:47 +0000

    idk what i did tbh

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,29 +3,14 @@ import { ref, onMounted } from 'vue'
 
 defineProps<{ msg: string }>()
 
-const count = ref(0)
-const input = ref<string>()
-
-const increment = () => {
-    count.value += 1
-}
-const decrement = () => {
-    count.value -= 1
-}
+const products = ref([{
+    code: 1,
+    name: "kippensoep",
+    category: "soep",
+    quantity: "quant"
+}])
 
 const upload = async (event: any) => {
-    console.log(event.files.length)
-    //we are going to looop through alle the files and 
-    // for (let i = 0; i < event.files.length; i++) {
-    //     let data = new FormData();
-    //     data.append('file', event.files[i])
-    //     await fetch('http://localhost:8080/upload', {
-    //         method: 'POST',
-    //         body: data
-    //     })
-    // }
-
-    //rewrite the the above code cleaner
     const data = new FormData()
     for (let i = 0; i < event.files.length; i++) {
         data.append('file', event.files[i])
@@ -45,17 +30,18 @@ onMounted(async () => {
 
 <template>
     <h1>{{ msg }}</h1>
-
-    <pButton label="increment" :onClick="increment" />
-    <pButton label="decrement" :onClick="decrement" />
-    <h1>{{ count }}</h1>
-    <pTextarea v-model="input" />
     <pFileUpload name="demo[]" @uploader="upload($event)" :multiple="true" customUpload accept="image/*"
         :maxFileSize="1000000">
         <template #empty>
             <p>Drag and drop files to here to upload.</p>
         </template>
     </pFileUpload>
+    <pDataTable class="my-3" :value="products" tableStyle="min-width: 50rem">
+        <pColumn field="code" header="Code"></pColumn>
+        <pColumn field="name" header="Name"></pColumn>
+        <pColumn field="category" header="Category"></pColumn>
+        <pColumn field="quantity" header="Quantity"></pColumn>
+    </pDataTable>
 </template>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,22 @@ import App from './App.vue'
 import PrimeVue from 'primevue/config'
 import 'primevue/resources/themes/lara-light-indigo/theme.css'
 import 'primevue/resources/primevue.min.css'
+import 'primeflex/primeflex.css'
 import Button from 'primevue/button'
 import Textarea from 'primevue/textarea'
 import FileUpload from 'primevue/fileupload'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+import ColumnGroup from 'primevue/columngroup' // optional
+import Row from 'primevue/row' // optional
 
 const app = createApp(App)
 app.use(PrimeVue)
 app.component('pButton', Button)
 app.component('pTextarea', Textarea)
 app.component('pFileUpload', FileUpload)
+app.component('pDataTable', DataTable)
+app.component('pColumn', Column)
+app.component('pColumnGroup', ColumnGroup)
+app.component('pRow', Row)
 app.mount('#app')
